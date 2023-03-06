66152956686fcdddc2dcf8754754268b102e4295
Alexander Shlyakhtin <aleksandr07011997@gmail.com>
2023-03-06 17:12:37 +0100

    first commit

---
+++ src/main/java/dev/alex/demo/cqrskafkademoapp/CqrsKafkaDemoAppApplication.java
@@ -0,0 +1,13 @@
+package dev.alex.demo.cqrskafkademoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsKafkaDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsKafkaDemoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/alex/demo/cqrskafkademoapp/CqrsKafkaDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alex.demo.cqrskafkademoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsKafkaDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
